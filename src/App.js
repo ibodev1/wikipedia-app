704fccf52d3a5876cb05361baffb09c699060d1d
ibodev1 <ibrahimodev42@gmail.com>
2022-12-25 09:50:52 +0300

    tailwindcss-palette-generator added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold text-primary">
+      Hello world!
+    </h1>
+    <h1 className="text-3xl font-bold text-dark">
       Hello world!
     </h1>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const getPalette = require("tailwindcss-palette-generator");
+
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: getPalette([
+        {
+          name: "primary",
+          color: "#134826",
+        },
+        {
+          name: "dark",
+          color: "#252525"
+        }
+      ])
+    },
   },
   plugins: [],
 }
