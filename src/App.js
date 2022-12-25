48a0bd30819bed027b967eaf942c912f880fa686
ibodev1 <ibrahimodev42@gmail.com>
2022-12-25 13:20:26 +0300

    making app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getData, getSearchList } from "./utilities";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [searchData, setSearchData] = useState([]);
+  useEffect(() => {
+    if (searchQuery.length > 2) {
+      setLoading(true);
+      getSearchList(searchQuery).then(queries => {
+        queries.forEach(query => {
+          getData(query).then(res => {
+            setSearchData(oldArray => [...oldArray, res.data]);
+            setLoading(false);
+          })
+        });
+      });
+    } else {
+      setLoading(false);
+      setSearchData([]);
+    }
+  }, [, searchQuery]);
+
+  if (!loading) {
+    console.log(searchData);
+  }
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold text-primary">
-      Hello world!
-    </h1>
-    <h1 className="text-3xl font-bold text-dark">
-      Hello world!
-    </h1>
+    <div className="container">
+      <input type="search" name="search" id="search" placeholder="Search..." required onChange={(e) => setSearchQuery(e.target.value)} />
+      {
+        loading ? "Loading..." : (
+          searchData.filter(elm => elm.type !== "no-extract").map(elm => {
+            return (
+              <>
+              <h1><b>{elm.title}</b></h1>
+              <p>{elm.description}</p>
+              </>
+            )
+          })
+        )
+      }
     </div>
   );
 }
