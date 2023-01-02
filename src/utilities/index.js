a9bdd2b9fe33e7ec95d827cd960c5f827c38cf92
ibodev1 <ibrahimodev42@gmail.com>
2023-01-02 17:43:42 +0300

    en to tr

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -6,14 +6,14 @@ const config = {
             'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/Summary/1.4.2"',
         "Content-Type":
             'application/json; charset=utf-8; profile="https://www.mediawiki.org/wiki/Specs/Summary/1.4.2"',
-        "Accept-Language": "tr-TR",
+        "Accept-Language": "en-US",
     }
 };
 
 export const getData = async (param) => {
     try {
         const newParam = encodeURIComponent(param);
-        const url = `https://tr.wikipedia.org/api/rest_v1/page/summary/${newParam}?redirect=true`;
+        const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${newParam}?redirect=true`;
         const res = await axios.get(url);
         return res;
     } catch (err) {
@@ -25,30 +25,10 @@ export const getData = async (param) => {
 export const getSearchList = async (param) => {
     try {
         const newParam = encodeURIComponent(param);
-        const url = `https://tr.wikipedia.org/w/api.php?action=opensearch&format=json&namespace=0&origin=*&search=${newParam}&limit=40`;
+        const url = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&namespace=0&origin=*&search=${newParam}&limit=10`;
         const res = await axios.get(url, config);
         return res.data[1];
     } catch (err) {
         console.error(err);
     }
 };
-
-// getSearchList("tür").then((res) => {
-//     console.log(res);
-//     res.forEach((element) => {
-//         getData(element).then((res) => {
-//             console.log("-----------------------");
-//             console.log(res.data.title);
-//             console.log(res.data.extract);
-//             console.log(
-//                 new Date(res.data.timestamp).toLocaleDateString("tr-TR", {
-//                     weekday: "long",
-//                     year: "numeric",
-//                     month: "long",
-//                     day: "numeric"
-//                 })
-//             );
-//             console.log("-----------------------");
-//         });
-//     });
-// });
